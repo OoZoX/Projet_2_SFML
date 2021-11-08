1337021d9159e340a0a59ec90812ef52a6f6c949
Quentin <bm.quentin@icloud.com>
2021-11-08 10:00:48 +0100

    all deplacement

--- sfml-formation-1/sfml-formation-1/sfml-1-tileset.cpp
+++ sfml-formation-1/sfml-formation-1/sfml-1-tileset.cpp
@@ -212,6 +212,18 @@ vector<vector<Sprite>> display_map(Texture& texture) {
 
 }
 
+void normalize(Vector2f& velocity, float speed) 
+{
+    
+    float norme = sqrt((velocity.x * velocity.x) + (velocity.y * velocity.y));
+    if (norme != 0)
+    {
+        velocity.x = ((velocity.x) / norme) * speed;
+        velocity.y = ((velocity.y) / norme) * speed;
+    }
+    
+}
+
 enum Keys { UP, DOWN, LEFT, RIGHT, SPACE, KEY_MAX };
 bool activeKeys[KEY_MAX] = { false };
 
@@ -229,6 +241,12 @@ int main()
     character.setScale(3, 3);
     character.setPosition(50, 50);
     float vit = 0.1f;
+
+
+    sf::Vector2f velocity;
+    float speed = 1.0f;
+
+
     while (window.isOpen())
     {
         sf::Event event;
@@ -239,28 +257,13 @@ int main()
             if (event.type == sf::Event::KeyPressed)
             {
                 if (event.key.code == sf::Keyboard::Q)
-                {
-                    activeKeys[LEFT] = true;
-                }
-
-
+                {activeKeys[LEFT] = true;}
                 if (event.key.code == sf::Keyboard::Z)
-                {
-                    activeKeys[UP] = true;
-                }
-
-
-
+                {activeKeys[UP] = true;}
                 if (event.key.code == sf::Keyboard::S)
-                {
-                    activeKeys[DOWN] = true;
-                }
-
-
+                {activeKeys[DOWN] = true;}
                 if (event.key.code == sf::Keyboard::D)
-                {
-                    activeKeys[RIGHT] = true;
-                }
+                {activeKeys[RIGHT] = true;}
 
             }
             if (event.type == sf::Event::KeyReleased)
@@ -271,18 +274,25 @@ int main()
                 if (event.key.code == sf::Keyboard::D) { activeKeys[RIGHT] = false; }
             }
         }
-        if (activeKeys[UP]) { character.move(0, -vit); }
-        if (activeKeys[UP] == false) { character.move(0, 0); }
+        if (activeKeys[UP] == false) { velocity.y -= velocity.y; cout << "false" << endl; }
+        if (activeKeys[LEFT] == false) { velocity.x -= velocity.x; }
+        if (activeKeys[RIGHT] == false) { velocity.x -= velocity.x; }
+        if (activeKeys[DOWN] == false) { velocity.y -= velocity.y; }
 
-        if (activeKeys[LEFT]) { character.move(-vit, 0); }
-        if (activeKeys[LEFT] == false) { character.move(0, 0); }
 
-        if (activeKeys[RIGHT]) { character.move(vit, 0); }
-        if (activeKeys[RIGHT] == false) { character.move(0, 0); }
+        if (activeKeys[UP]) { velocity.y -= speed; cout << "--------aa" << velocity.y << "----------" << endl; }
+        if (activeKeys[LEFT]) {velocity.x -= speed; }
+        if (activeKeys[RIGHT]) { velocity.x += speed; }
+        if (activeKeys[DOWN]) { velocity.y += speed; cout << "--------bb" << velocity.y << "----------" << endl;
+        }
+        
 
-        if (activeKeys[DOWN]) { character.move(0, vit); }
-        if (activeKeys[DOWN] == false) { character.move(0, 0); }
 
+        cout << velocity.x << velocity.y << endl;
+        //normalize(velocity, speed);
+        character.move(velocity);
+
+        /*
         if (activeKeys[UP] && activeKeys[LEFT])
         {
             character.move(vit * 0.29, vit * 0.29);
@@ -299,7 +309,9 @@ int main()
         {
             character.move(-vit * 0.29, -vit * 0.29);
         }
+        */
 
+        /*
         if (character.getPosition().x <= 0)
         {
             character.setPosition(0, character.getPosition().y);
@@ -316,6 +328,10 @@ int main()
         if (character.getPosition().y >= 580) {
             character.setPosition(character.getPosition().x, 580);
         }
+
+
+        */
+
         for (int i = 0; i < recup_map[1].size(); i++)
         {
             window.draw(recup_map[1][i]);
@@ -327,9 +343,108 @@ int main()
         window.draw(character);
         window.display();
         window.clear();
-        }
+    }
 
         
 
+
     return 0;
 }
+
+/**
+
+#include <SFML/Graphics.hpp>
+#include <iostream>
+#include <Windows.h>
+
+
+using namespace sf;
+using namespace std;
+
+void normalize(Vector2f& velocity, float speed) {
+    float norme = sqrt((velocity.x * velocity.x) + (velocity.y * velocity.y));
+    if (norme != 0)
+    {
+        velocity.x = ((velocity.x) / norme) * speed;
+        velocity.y = ((velocity.y) / norme) * speed;
+    }
+}
+
+int main()
+{
+    int dim = 16; // Ne pas changer (taille des tiles de foresttiles2-t.png)
+    int scale = 4;
+
+#pragma region INIT
+
+    sf::RenderWindow window(sf::VideoMode((dim * scale * 12), (dim * scale * 8)), "The game seems to be working..."); // La map possede 12 colones et 8 lignes
+    window.setKeyRepeatEnabled(false);
+
+#pragma region Player
+    sf::RectangleShape entity(sf::Vector2f(((dim - 4) * scale), (dim * scale)));
+    Texture texture;
+    texture.loadFromFile("ghost.png");
+    Sprite sprite;
+    entity.setTexture(&texture);
+    sf::Vector2f velocity;
+    float speed = 0.5;
+#pragma endregion
+
+#pragma region Map
+    Tilemap T;
+    Texture maptexture;
+    maptexture.loadFromFile("foresttiles2-t.png");
+    vector<Sprite> vecground = T.loadGround(dim, scale, maptexture);
+    vector<Sprite> vecmap = T.loadLevel(dim, scale, maptexture);
+#pragma endregion
+
+#pragma endregion
+
+    while (window.isOpen())
+    {
+        Event event;
+        while (window.pollEvent(event))
+        {
+            if (event.type == Event::Closed) { window.close(); }
+            if (event.type == Event::KeyReleased)
+            {
+                switch (event.key.code)
+                {
+                case Keyboard::Z:
+                    velocity.y -= velocity.y;
+                    break;
+                case Keyboard::Q:
+                    velocity.x -= velocity.x;
+                    break;
+                case Keyboard::S:
+                    velocity.y -= velocity.y;
+                    break;
+                case Keyboard::D:
+                    velocity.x -= velocity.x;
+                    break;
+                }
+            }
+        }
+
+        if (Keyboard::isKeyPressed(Keyboard::Z)) { velocity.y -= speed; }
+        if (Keyboard::isKeyPressed(Keyboard::Q)) { velocity.x -= speed; }
+        if (Keyboard::isKeyPressed(Keyboard::S)) { velocity.y += speed; }
+        if (Keyboard::isKeyPressed(Keyboard::D)) { velocity.x += speed; }
+
+        normalize(velocity, speed);
+        entity.move(velocity); 
+        cout << "Velocity " + to_string(velocity.x) + " | " + to_string(velocity.y) + "\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n";
+
+#pragma region Draw
+        window.clear();
+        for (int i = 0; i < vecground.size(); i++) { window.draw(vecground[i]); }
+        for (int i = 0; i < vecmap.size(); i++) { window.draw(vecmap[i]); }
+        window.draw(entity);
+        window.draw(sprite);
+        window.display();
+#pragma endregion
+    }
+    return 0;
+}
+
+*/
